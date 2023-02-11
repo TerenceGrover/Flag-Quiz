d13556b190caf8711310fe7547cc644974fd727d
TerenceGrover <terencegrover@gmail.com>
2023-02-11 19:01:42 +0100

    refactor: Refacotr into functions

--- Flagquiz.js
+++ Flagquiz.js
@@ -1,90 +1,93 @@
-let score = 0
-let scoretxt = document.querySelector('#score')
-let high = 0
-let hightxt = document.querySelector('#highscore')
-let life = 3
-let lives = document.querySelector('#lives')
-let soltxt = document.querySelector('#solution')
+const hightxt = document.querySelector('#highscore');
+const scoretxt = document.querySelector('#score');
+const lives = document.querySelector('#lives');
+const soltxt = document.querySelector('#solution');
 
-fetch('https://restcountries.com/v3.1/all')
-.then(response => response.json())
-.then(data => {
-    let count = (data[rando()])
-    image.src = count.flags.png
-    let solution = count.name.common.toLowerCase()
-    soltxt.textContent = solution
-    let alt = count.name.common.toLowerCase().split(' ')
-    console.log(alt)
-    console.log(solution)
+let countries;
 
-    function refresh(){
-    count = (data[rando()])
-    image.src = count.flags.png
-    solution = count.name.common.toLowerCase()
-    soltxt.textContent = solution
-    console.log(solution)
-    }
+let score = 0;
+let high = 0;
+let life = 3;
 
-    function reset(){
-        score = 0
-        life = 3
-        scoretxt.textContent = 'Score: ' + score
-        lives.textContent= 'Life Counter : ✅ ✅ ✅'
-        guess.value = ''
-        soltxt.style.opacity = "0"
-        refresh()
-    }
+let image = document.querySelector('img');
+let guess = document.querySelector('input');
 
-    subm.addEventListener('click', () => {
-        let x = guess.value.toLowerCase()
-        if (x == solution || x == alt[0] || x== alt[alt.length-1]){
-            score++
-            scoretxt.textContent = 'Score: ' + score
-            guess.value = ''
-            x = ''
-            refresh()
-        }else {
-            liferundown()
-        }})
-        function liferundown(){
-            life -= 1
-            switch (life) {
-            case 2:
-                lives.textContent = 'Life Counter : ✅ ✅ ❌';
-                break;
-            case 1:
-                lives.textContent = 'Life Counter : ✅ ❌ ❌';
-                break;
-            case 0:
-                lives.textContent = 'Life Counter : ❌ ❌ ❌';
-                soltxt.style.opacity = "1"
-                setTimeout(() => {
-                    high = score
-                    hightxt.textContent = 'Highscore: ' + high
-                    reset()
-                }, 2000);
-                break;
-            
-            console.log(life)
-        }
-    }
-    skip.addEventListener('click', () => {
-        soltxt.style.opacity = "1"
-        setTimeout(() => {
-            refresh()
-            liferundown()
-            soltxt.style.opacity = "0"
-        }, 2000);
-    })
-})
-
-let image = document.querySelector('img')
-let subm = document.querySelector('button')
-let skip = document.querySelector('#skip')
-let guess = document.querySelector('input')
-
-function rando(){
-    let x = Math.ceil(Math.random()*249)
-    return x
+function rando() {
+  let x = Math.ceil(Math.random() * 249);
+  return x;
 }
 
+(async function init() {
+  countries = await (
+    await fetch('https://restcountries.com/v3.1/all?fields=name,flags')
+  ).json();
+  let count = countries[rando()];
+  image.src = count.flags.png;
+  let solution = count.name.common.toLowerCase();
+  soltxt.textContent = solution;
+
+  document.querySelector('button').addEventListener('click', () => {
+    let x = guess.value.toLowerCase();
+    if (x == solution) {
+      score++;
+      scoretxt.textContent = 'Score: ' + score;
+      guess.value = '';
+      x = '';
+      refresh();
+    } else {
+      reveal();
+    }
+  })
+
+  document.querySelector('#skip').addEventListener('click', () => {
+    reveal();
+  });
+})();
+
+function refresh() {
+  count = countries[rando()];
+  image.src = count.flags.png;
+  solution = count.name.common.toLowerCase();
+  soltxt.textContent = solution;
+  console.log(solution);
+}
+
+function reset() {
+  score = 0;
+  life = 3;
+  scoretxt.textContent = 'Score: ' + score;
+  lives.textContent = 'Life Counter : ✅ ✅ ✅';
+  guess.value = '';
+  soltxt.style.opacity = '0';
+  refresh();
+}
+
+function reveal() {
+  soltxt.style.opacity = '1';
+  setTimeout(() => {
+    refresh();
+    liferundown();
+    soltxt.style.opacity = '0';
+  }, 2000);
+}
+
+function liferundown() {
+  life -= 1;
+  switch (life) {
+    case 2:
+      lives.textContent = 'Life Counter : ✅ ✅ ❌';
+      break;
+    case 1:
+      lives.textContent = 'Life Counter : ✅ ❌ ❌';
+      break;
+    case 0:
+      lives.textContent = 'Life Counter : ❌ ❌ ❌';
+      soltxt.style.opacity = '1';
+      setTimeout(() => {
+        high = score || high;
+        hightxt.textContent = 'Highscore: ' + high;
+        reset();
+      }, 2000);
+      break;
+  }
+}
--- README.md
+++ README.md
@@ -6,6 +6,4 @@ All coded in native JavaScriptHTML/CSS
 
 Utlimately, a better graphic interface will be used, the highscore will be stored on the browser memory and you will be able to have simultaneous online games.
 
-(Terrible in-line CSS)
-
 <img width="1657" alt="Capture d’écran 2022-08-16 à 18 26 12" src="https://user-images.githubusercontent.com/109336882/184930514-bae57059-2581-460b-a609-9fce37ef5caf.png">
